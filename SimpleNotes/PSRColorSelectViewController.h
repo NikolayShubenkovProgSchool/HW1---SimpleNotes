fb554711c2c2be1d99bd3f1df040555f851c1059
Dmitry Arbuzov <dm@watm.ru>
2014-10-03 19:04:43 +0400

    Added PSRColorSelectViewController class + view. Added action to change backgroundColor reacting on sliders. Next step - Segue from detailViewController, changing note text color and saving text color in model.

---
+++ SimpleNotes/PSRColorSelectViewController.h
@@ -0,0 +1,22 @@
+//
+//  PSRColorSelectViewController.h
+//  SimpleNotes
+//
+//  Created by Dmitry Arbuzov on 03.10.14.
+//  Copyright (c) 2014 Daniil Korotin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PSRColorSelectViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIView *colorView;
+
+@property (weak, nonatomic) IBOutlet UISlider *redSlider;
+@property (weak, nonatomic) IBOutlet UISlider *greenSlider;
+@property (weak, nonatomic) IBOutlet UISlider *blueSlider;
+
+- (IBAction)sliderValueChanged:(UISlider *)sender;
+
+
+@end
---
+++ SimpleNotes/PSRColorSelectViewController.m
@@ -0,0 +1,36 @@
+//
+//  PSRColorSelectViewController.m
+//  SimpleNotes
+//
+//  Created by Dmitry Arbuzov on 03.10.14.
+//  Copyright (c) 2014 Daniil Korotin. All rights reserved.
+//
+
+#import "PSRColorSelectViewController.h"
+
+@interface PSRColorSelectViewController ()
+
+@end
+
+@implementation PSRColorSelectViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    [self sliderValueChanged:nil];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Actions
+
+- (IBAction)sliderValueChanged:(UISlider *)sender {
+    self.colorView.backgroundColor = [UIColor colorWithRed:self.redSlider.value
+                                                      green:self.greenSlider.value
+                                                       blue:self.blueSlider.value
+                                                      alpha:1.0];
+}
+@end

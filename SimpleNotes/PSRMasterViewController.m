7de1475aac9b447abc8abc4498efb0134f774bc6
Dmitry Arbuzov <dm@watm.ru>
2014-10-03 18:27:57 +0400

    Deleted empty method initWithNibName: StoryboardImplementation is ended.

--- SimpleNotes/PSRMasterViewController.m
+++ SimpleNotes/PSRMasterViewController.m
@@ -16,18 +16,10 @@
 
 @implementation PSRMasterViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
 - (void)viewDidLoad {
     [super viewDidLoad];
 }
+
 - (IBAction)addNote:(UIBarButtonItem *)sender {
     PSRNote *note = [[PSRNote alloc] init];
     [[PSRNoteManager sharedManager] addOrUpdateNote:note];

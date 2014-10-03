1b394875373808e095dbd510f47dd8f0cc558b3e
Dmitry Arbuzov <dm@watm.ru>
2014-10-04 00:40:21 +0400

    Added PSRFontSelectViewController and view. Shows all fonts

---
+++ SimpleNotes/PSRFontSelectViewController.h
@@ -0,0 +1,13 @@
+//
+//  PSRFontSelectViewController.h
+//  SimpleNotes
+//
+//  Created by Dmitry Arbuzov on 04.10.14.
+//  Copyright (c) 2014 Daniil Korotin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PSRFontSelectViewController : UITableViewController
+
+@end
---
+++ SimpleNotes/PSRFontSelectViewController.m
@@ -0,0 +1,61 @@
+//
+//  PSRFontSelectViewController.m
+//  SimpleNotes
+//
+//  Created by Dmitry Arbuzov on 04.10.14.
+//  Copyright (c) 2014 Daniil Korotin. All rights reserved.
+//
+
+#import "PSRFontSelectViewController.h"
+
+@interface PSRFontSelectViewController ()
+
+@property (strong, nonatomic) NSArray *familyNames;
+
+@end
+
+@implementation PSRFontSelectViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.familyNames = [UIFont familyNames];
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return self.familyNames.count;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    NSString *familyName = self.familyNames[section];
+    NSArray *fontNames = [UIFont fontNamesForFamilyName:familyName];
+    return fontNames.count;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    NSString *familyName = self.familyNames[section];
+    return familyName;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *identifier = @"cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
+    }
+    
+    NSString *familyName = self.familyNames[indexPath.section];
+    NSArray *fontNames = [UIFont fontNamesForFamilyName:familyName];
+    NSString *fontName = fontNames[indexPath.row];
+    
+    cell.textLabel.text = fontName;    
+    UIFont *font = [UIFont fontWithName:fontName size:16];
+    cell.textLabel.font = font;
+    
+    return cell;
+}
+
+@end

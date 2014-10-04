1fd927c2c0ba7a3f98bea11a61de2139b2d86af0
Nikolay Shubenkov <n.shubenkov@gmail.com>
2014-10-04 17:18:55 +0400

    create(UIColor+PSRComponents)category for colour components. Not Implemented

---
+++ SimpleNotes/UIColor+PSRComponents.h
@@ -0,0 +1,17 @@
+//
+//  UIColor+PSRComponents.h
+//  SimpleNotes
+//
+//  Created by n.shubenkov on 04/10/14.
+//  Copyright (c) 2014 Daniil Korotin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (PSRComponents)
+
+- (CGFloat)psr_redComponent;
+- (CGFloat)psr_greenComponent;
+- (CGFloat)psr_blueComponent;
+
+@end
---
+++ SimpleNotes/UIColor+PSRComponents.m
@@ -0,0 +1,15 @@
+//
+//  UIColor+PSRComponents.m
+//  SimpleNotes
+//
+//  Created by n.shubenkov on 04/10/14.
+//  Copyright (c) 2014 Daniil Korotin. All rights reserved.
+//
+
+#import "UIColor+PSRComponents.h"
+
+@implementation UIColor (PSRComponents)
+
+//опишите их самостоятельно
+
+@end
